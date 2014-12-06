450d6ca3e2455658d47a4546cb77551c24d77fa4
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2014-12-06 17:07:29 +0000

    add user to user table if not already in it (raven)

--- assets/passport-config.js
+++ assets/passport-config.js
@@ -1,12 +1,15 @@
 /*jshint unused:true, bitwise:true, eqeqeq:true, undef:true, latedef:true, eqnull:true */
 /* global require, console */
 
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var RavenStrategy = require('passport-raven').Strategy;
+var passport       = require('passport');
+var LocalStrategy  = require('passport-local').Strategy;
+var RavenStrategy  = require('passport-raven').Strategy;
 var BearerStrategy = require('passport-http-bearer').Strategy;
-var jwt = require("jwt-simple");
-var __ = require("./strings.js");
+var jwt            = require("jwt-simple");
+var __             = require("./strings.js");
+var mysql          = require("mysql");
+var https           = require("https");
+
 var secret = "---top-secret-string---";
 
 function getToken(userId) {
@@ -45,13 +48,47 @@ passport.use(new LocalStrategy(
 // configure raven passport authentication strategy
 passport.use(new RavenStrategy({
 	// NB I don't know what audience is actually for. It seems to just work like a base url
-	audience: 'http://localhost:2000/api',
+	audience: 'http://localhost:2000',
 	desc: __('Mabel Ticketing System'),
 	msg: __('Mabel needs to check you are a current member of Emmanuel College'),
 	// use demonstration raven server in development
 	debug: false
-}, function(crsid, params, callback) {
-	callback(null, {
+}, function(crsid, params, done) {
+	// connect to database
+	var conn = mysql.createConnection({
+		host: '104.236.25.186',
+		user: 'chris',
+		password: 'mabel-dbCHRISTMAS',
+		database: 'mabel'
+	});
+	conn.connect();
+
+	// try to find user in db with crsid
+	conn.query("SELECT * FROM user WHERE crsid='" + crsid + "'", function(err, rows) {
+		if (err) console.log(err);
+		if (rows.length == 0) {
+			// if user not in table then put them in
+			var lookupURL = "https://anonymous:@lookup-test.csx.cam.ac.uk/api/v1/person/crsid/" + crsid+ "?format=json";
+			https.get(lookupURL, function(res) {
+				var body = '';
+				res.on('data', function(chunk) {
+					body += chunk;
+				});
+				res.on('end', function() {
+					var response = JSON.parse(body);
+					var name = response.result.person.displayName;
+					conn.query("INSERT INTO user (name, email, crsid,\
+					 registration_time) VALUES ('"+name+"','"+crsid+"@cam.ac.uk','"+crsid+"', CURRENT_TIMESTAMP)");
+					conn.commit(function(err) { if (err) throw err; });
+				});
+			}).on('error', function(e) {
+				  console.log("Got error: ", e);
+			});
+		}
+	});
+
+	// call callback
+	done(null, {
 		token: getToken(crsid)
 	});
 }));
