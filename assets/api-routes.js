4b673b0994459ac2f414aaa8fae43d3f7c1e6cfc
Christopher Little <hephistocles@gmail.com>
2014-12-06 12:26:26 +0000

    Fix location of strings.js

--- assets/api-routes.js
+++ assets/api-routes.js
@@ -2,8 +2,7 @@
 var express = module.parent.exports.express;
 
 var router = express.Router();
-var __ = require("./assets/strings.js");
-
+var __ = require("../assets/strings.js");
 
 router.get("/", function(req, res) {
 	res.json({"Welcome" : __("welcome to the api")});
