8b547b6fd93f86125f568a8c7b0ab7836ecffe12
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2014-12-06 11:54:47 +0000

    app root module

---
+++ app.js
@@ -0,0 +1,21 @@
+// imports
+var express = require("express");
+var app     = express();
+
+// make visible outside module
+module.exports.express = express;
+module.exports.app     = app;
+
+// import string module
+var __ = require("./assets/strings.js");
+
+
+/* ROUTES */
+
+app.get("/", function(req,res) {
+	res.send(__("You have joined the queue."));
+})
+
+
+// listen on port 2000
+app.listen(2000);
