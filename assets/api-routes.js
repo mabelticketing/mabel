fe4035e09ea3cf22f558946d303a4b50bc5b874d
Christopher Little <hephistocles@gmail.com>
2014-12-06 12:22:35 +0000

    Add localisation development settings (count uses)

--- assets/api-routes.js
+++ assets/api-routes.js
@@ -2,9 +2,11 @@
 var express = module.parent.exports.express;
 
 var router = express.Router();
+var __ = require("./assets/strings.js");
+
 
 router.get("/", function(req, res) {
-	res.json({"Welcome" : "welcome to the api"});
+	res.json({"Welcome" : __("welcome to the api")});
 });
 
 module.exports = router;
--- assets/strings.js
+++ assets/strings.js
@@ -4,10 +4,22 @@
 var res = {
 	"You have joined the queue.": "You have now joined the queue. dfgskjdfgkjdsfgkj"
 };
+var counts = {};
 
 function getString(baseString) {
 	// if it's defined, get an alternative base string
 	baseString = res[baseString] || baseString;
+
+	// for development, add the new string to the 'res' in memory so that we can dump it and see what strings are used
+	if (res[baseString] === undefined) {
+		res[baseString] = baseString;
+	}
+	if (counts[baseString] === undefined) {
+		counts[baseString] = 0;
+	}
+	counts[baseString]++;
+	// end dev section
+	
 
 	if (arguments.length > 1) {
 		// some parameters have been passed
@@ -17,6 +29,7 @@ function getString(baseString) {
 			baseString = baseString.replace("%s", arguments[i].toString());
 		}	
 	}
+
 	return baseString;
 }
 
