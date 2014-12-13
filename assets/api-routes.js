707bd9d3570daddc3bce33dcf6412adbd46bd580
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2014-12-13 10:22:43 +0000

    commit for merge

--- assets/api-routes.js
+++ assets/api-routes.js
@@ -6,6 +6,10 @@ var express = module.parent.exports.express;
 var router = express.Router();
 var __ = require("./strings.js");
 var passport = require("passport");
+var mysql = require('mysql');
+var config = require('./config.js');
+// TODO: send every route through passport.authenticate
+//       rather than doing it on each route individually
 
 router.get("/", function(req, res) {
 	res.json({
@@ -35,6 +39,30 @@ router.get("/buy",
 	}
 );
 
+var conn = mysql.createConnection({
+		host: config.db_host,
+		user: config.db_user,
+		password: config.db_password,
+		database: config.db_db
+	});
 
+router.get("/user",
+	passport.authenticate('bearer', {
+		session: false
+	}),
+	function(req, res) {
+		var conn = mysql.createConnection({
+			host: config.db_host,
+			user: config.db_user,
+			password: config.db_password,
+			database: config.db_db
+		});
+		conn.query("SELECT * FROM user WHERE id=?", [req.params.id], function(err, rows) {
+			if (err) res.send(err);
+			var user = rows[0];
+			res.json(user);
+		});
+		conn.end();
+	});
 
 module.exports = router;
--- assets/app-routes.js
+++ assets/app-routes.js
@@ -3,9 +3,11 @@ var express = module.parent.exports.express;
 
 var router = express.Router();
 var passport = require("passport");
+var http = require('http');
+
 
 router.get("/", function(req, res) {
-	res.render("index.html");
+	res.render("index.jade");
 });
 
 router.get('/login/mabel',
@@ -29,5 +31,40 @@ router.get('/login/raven',
 		});
 	}
 );
+
+router.get('/book', function(req, res) {
+	// TODO: complete this route.
+	// check user did pass through queue
+	// get user information
+	// check what tickets are available to user
+	// get ticket information, prices etc
+	// send to page
+	var user_at_front_of_queue = true; //TODO: check queue position etc etc
+	if (user_at_front_of_queue) {
+		// do stuff here
+
+		// GET /api/user - basically just for user.name i think
+
+		// GET /api/ticketinfo (Large object (searching multiple tables) - hope its okay)
+
+		// create data object & res.render, sending data with it
+
+res.render('book.jade');
+
+	} else res.redirect('/queue'); // if queue position not okay redirect to /queue
+
+	
+
+
+	
+	// will need to allow users to select tickets available to them, and then hit "book",
+	// and then be redirected to a confirmation page, hopefully displaying tickets they
+	// booked, along with a "what happens next" paragraph e.g. will receive a confirmation email.
+});
+
+router.get('/confirmation', function(req, res) {
+	// TODO: complete this route
+	res.render('confirmation.jade');
+});
 
 module.exports = router;
