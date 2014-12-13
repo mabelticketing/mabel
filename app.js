9f1ac2033b79b0e17ea2caa6cac41dd13da90637
Christopher Little <hephistocles@gmail.com>
2014-12-13 14:49:14 +0000

    Start work on admin section and refactor API to separate from api endpoints

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var __ = require("./src/strings.js");
 app.set("views", __dirname + "/views");
 
 // set the default template engine to ejs - for static html
+app.locals.pretty = true;
 app.engine('jade', require('jade').__express);
 
 /* ROUTES */
@@ -26,10 +27,9 @@ var APPRouter = require("./src/app-routes.js");
 var APIRouter = require("./src/api-routes.js");
 
 app.use(passport.initialize());
-// passport session relies on express session
-// app.use(passport.session());
 app.use("/", APPRouter);
 app.use("/api", APIRouter);
+app.use('/assets', express.static(__dirname + '/assets'));
 
 // listen on port 2000
 app.listen(config.port || 2000);
