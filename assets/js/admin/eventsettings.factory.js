0878316f56f218ae0d555f8c588815519ecda521
Christopher Little <hephistocles@gmail.com>
2014-12-27 22:43:26 +0000

    Start using unix timestamps for transport and storage
    Note that schema needs to be reset after this change

--- assets/js/admin/eventsettings.factory.js
+++ assets/js/admin/eventsettings.factory.js
@@ -2,49 +2,112 @@
 angular.module('mabel.admin')
 	.factory('EventSettings', EventSettings);
 
-function EventSettings($resource, $timeout, $rootScope, MabelToken) {
+function serializeTime(date) {
+	if (date !== undefined) return date.unix();
+}
+function unserializeTime(dateString) {
+	if (dateString !== undefined) return moment.unix(dateString);
+}
+function unserializeEvent(event) {
+	if (event === undefined) return event;
+	var ev = {};
+	ev.launch_time = unserializeTime(event.launch_time);
+	ev.close_time = unserializeTime(event.close_time);
+	ev.name = event.name;
+	ev.id = event.id;
+	return ev;
+}
+function serializeEvent(event) {
+
+	if (event === undefined) return event;
+	var ev = {};
+	ev.launch_time = serializeTime(event.launch_time);
+	ev.close_time = serializeTime(event.close_time);
+	ev.name = event.name;
+	ev.id = event.id;
+	return ev;
+}
+
+function EventSettings($resource, $timeout, $rootScope, MabelToken, $http) {
 	
-	var Settings = $resource('/api/event/:id', {
+	var EventSettingsResource = $resource('/api/event/:id', {
 		access_token: MabelToken,
 		id: '@id'
-	});
+	},
+	{
+		'_get': {
+			method: 'GET',
+			transformRequest: [serializeEvent].concat($http.defaults.transformRequest),
+			transformResponse: $http.defaults.transformResponse.concat([unserializeEvent])
+		},
+		'save': {
+			method: 'POST',
+			transformRequest: [serializeEvent].concat($http.defaults.transformRequest),
+			transformResponse: $http.defaults.transformResponse.concat([unserializeEvent])
+		}
+	}
+	);
 
-	return {
-		get: get
-	};
+	EventSettingsResource.get = get;
+	EventSettingsResource.prototype.delete = deleteIt;
+	EventSettingsResource.prototype.save = save;
 
-	function get(obj, callbacks) {
-		var successCallback = callbacks.success || function() {};
-		var loadingCallback = callbacks.loading || function() {};
-		var errorCallback = callbacks.error || function() {};
+	return EventSettingsResource;
 
-		var settings = Settings.get(obj, function() {
+	function get(parameters, success, error) {
+		return EventSettingsResource._get(parameters, function(value, responseHeaders) {
+
 			// start watching so we can update the server
 			var stopTimer;
+			function compare(A, B) {
+				return A.name === B.name &&
+					((A.launch_time.isSame !== undefined && A.launch_time.isSame(B.launch_time, 'minutes')) ||
+					(B.launch_time.isSame !== undefined && B.launch_time.isSame(A.launch_time, 'minutes')));
+			}
+			
+			function getValue() {
+				return value;
+			}
+			$rootScope.$watch(getValue, change(stopTimer, compare), true);
 
-			settings.launch_time = moment(settings.launch_time);
-			settings.modification_stop_time = moment(settings.modification_stop_time);
+			// define non-enumerable properties so it doesn't show up in the DB
+			Object.defineProperty(value, '_status', { value: '', enumerable: false, writable: true });
+			Object.defineProperty(value, '_error', { value: '', enumerable: false, writable: true });
 
-			$rootScope.$watch(function() {
-				return settings;
-			}, function(newV, old) {
-				// I'm only interested in saving on actual changes
-				if (old === newV) return;
-				// need to check individual properties too because the above just checks object references
-				if (old.name === newV.name &&
-					((old.launch_time.isSame !== undefined && old.launch_time.isSame(newV.launch_time, 'minutes')) ||
-					(newV.launch_time.isSame !== undefined && newV.launch_time.isSame(old.launch_time, 'minutes'))) 
-					) return;
-
-				if (stopTimer !== undefined) $timeout.cancel(stopTimer);
-				// only update server periodically else we'll make too many calls
-				stopTimer = $timeout(function() {
-					loadingCallback();
-					settings.$save(successCallback, errorCallback);
-				}, 500);
-			}, true);
-
+			if (success !== undefined) success(value, responseHeaders);
+		}, error);
+	}
+	function deleteIt(success, error) {
+		if (typeof this._clearWatch === "function") this._clearWatch();
+		this.$delete(success, error);
+	}
+	function save(success, error) {
+		this._status = "pending";
+		this._error = "";
+		var resource = this;
+		this.$save(function() {
+			resource._status = "success";
+			if (success !== undefined) success(resource);
+		}, function(response) {
+			resource._status = "error";
+			resource._error = response.data;
+			if (error !== undefined) error(response.data);
 		});
-		return settings;
+	}
+	function change(stopTimer, areEqual) {
+		return function(newValue, oldValue) {
+			// I'm only interested in saving on actual changes
+			if (oldValue === newValue) return;
+
+			// need to check details as well, because === just compares refs
+			if (areEqual(oldValue, newValue)) return;
+
+			if (stopTimer !== undefined) $timeout.cancel(stopTimer);
+
+			// only update server periodically else we'll make too many calls
+			stopTimer = $timeout(function() {
+				newValue.save();
+			}, 500);
+		};
 	}
 }
--- sql/defaults.sql
+++ sql/defaults.sql
@@ -1,8 +1,8 @@
-insert into mabel.event (id, name, launch_time, modification_stop_time) VALUES (
+insert into mabel.event (id, name, launch_time, close_time) VALUES (
 	1,
 	'Emmanuel May Ball 2015', 
-	'2014-12-13 00:00:00',
-	'2014-12-25 00:00:00'
+	UNIX_TIMESTAMP('2014-12-13 00:00:00'),
+	UNIX_TIMESTAMP('2014-12-25 00:00:00')
 );
 
 insert into mabel.ticket_type (id, name, price, ticket_limit, event_id) VALUES (
@@ -32,6 +32,27 @@ insert into mabel.ticket_status (name) VALUES ('Pending');
 insert into mabel.ticket_status (name) VALUES ('Confirmed');
 insert into mabel.ticket_status (name) VALUES ('Cancelled');
 insert into mabel.ticket_status (name) VALUES ('Admitted');
+
+insert into mabel.user (id, name, email, crsid, registration_time) VALUES (
+	1,
+	'Christopher Little',
+	'cl554@cam.ac.uk',
+	'cl554',
+	UNIX_TIMESTAMP(NOW())
+);
+insert into mabel.user (id, name, email, crsid, registration_time) VALUES (
+	2,
+	'Thomas Le Feuvre',
+	'tl368@cam.ac.uk',
+	'tl368',
+	UNIX_TIMESTAMP(NOW())
+);
+insert into mabel.user (id, name, email, registration_time) VALUES (
+	3,
+	'Andrew Hardwurk',
+	'worker@clittle.com',
+	UNIX_TIMESTAMP(NOW())
+);
 
 insert into mabel.user_group (id, name, description) VALUES (
 	1,
@@ -64,7 +85,15 @@ insert into mabel.user_group (id, name, description) VALUES (
 	'Performers at the ball'
 );
 
-insert into mabel.group_access_right (id, group_id, ticket_type_id) VALUES (1,2,1);
+insert into mabel.user_group_membership (user_id, group_id) VALUES (1, 1);
+insert into mabel.user_group_membership (user_id, group_id) VALUES (2, 1);
+insert into mabel.user_group_membership (user_id, group_id) VALUES (1, 2);
+insert into mabel.user_group_membership (user_id, group_id) VALUES (2, 2);
+insert into mabel.user_group_membership (user_id, group_id) VALUES (1, 3);
+insert into mabel.user_group_membership (user_id, group_id) VALUES (2, 3);
+insert into mabel.user_group_membership (user_id, group_id) VALUES (3, 6);
+
+insert into mabel.group_access_right (id, group_id, ticket_type_id) VALUES (1,1,1);
 insert into mabel.group_access_right (id, group_id, ticket_type_id) VALUES (2,2,2);
 insert into mabel.group_access_right (id, group_id, ticket_type_id) VALUES (3,4,1);
 insert into mabel.group_access_right (id, group_id, ticket_type_id) VALUES (4,6,3);
