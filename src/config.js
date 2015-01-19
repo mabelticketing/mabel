b7d403069566d70cd8b1e6a16b08978c2cb96961
Christopher Little <hephistocles@gmail.com>
2015-01-19 13:12:47 +0000

    Re-anonymise config (belatedly?)

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
 module.exports = {
-    port        : 3008,
-    base_url    : "http://localhost:3008",
-    lookup_url  : "https://anonymous:@lookup-test.csx.cam.ac.uk/api/v1/",
-    db_host     : "104.236.25.186",
-    db_password : "mabel-dbCHRISTMAS",
-    db_user     : "chris",
-    db_db       : "mabel",
-    jwt_secret  : "---my top secret secret---"
+	port : 3008,
+	base_url : "http://localhost:3008",
+	lookup_url : "https://anonymous:@lookup-test.csx.cam.ac.uk/api/v1/",
+	jwt_secret : "",
+	// db_host : "",
+	// db_user : "",
+	// db_password : "",
+	// db_db : "",
+	db_host : "localhost",
+	db_user : "",
+	db_password : "",
+	db_db : "",
 };
-
