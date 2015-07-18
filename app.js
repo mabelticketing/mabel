fcc88bb2a2a8f3f0d952e453518da955e5668d80
Christopher Little <hephistocles@gmail.com>
2015-07-18 17:27:37 +0100

    Remove grunt in favour of bash/npm

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express   = require("express");
 var app       = express();
 var passport  = require("passport");
 var config    = require('./src/config.js');
-// var Cluster   = require('cluster2');
+
+// bail if it doesn't look like we've got a real config
+if (!config.port) throw new Error("I don't think you've initialised the config.");
+
 require("./src/passport-config.js");
 
 // make visible outside module
@@ -28,7 +31,14 @@ app.use("/api", APIRouter);
 // serve static content in assets from root
 app.use('/', express.static(__dirname + '/assets'));
 
+ /****************************************************************************** 
+ * If you want to use clustering (run as many instances as you have processors
+ * and share the load between them), uncomment the following chunk. You'll
+ * need to install cluster2 though -- which I had difficulty with on some
+ * machines.
+ *******************************************************************************/
 
+// var Cluster   = require('cluster2');
 // var c = new Cluster({
 	// port: config.port || 2000
 // });
@@ -36,5 +46,9 @@ app.use('/', express.static(__dirname + '/assets'));
 	// cb(app);
 // });
 
+
+ /*********************************************
+ * This is the regular (non-clustered) option
+ *********************************************/
 
 app.listen(config.port || 2000);
