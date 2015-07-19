40a0c47e4ec13828c4f2172bfe9383af5453cb6c
Christopher Little <hephistocles@gmail.com>
2015-07-19 09:13:20 +0100

    Use the mabel fork of JsTyper in bower rather than the original

--- assets/js/admin/barcode.controller.js
+++ assets/js/admin/barcode.controller.js
@@ -3,5 +3,5 @@ angular.module('mabel.admin')
 	.controller("BarcodeController", BarcodeController);
 
 function BarcodeController() {
-	JsBarcode(document.getElementById("barcode"), "123456789005", {format:"UPC",displayValue:true,fontSize:20})
+	JsBarcode(document.getElementById("barcode"), "12345 45678", {format:"UPC",displayValue:true,fontSize:20})
 }
