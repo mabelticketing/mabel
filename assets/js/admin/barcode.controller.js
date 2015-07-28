184788dc40b8a28036fdc0fd72bdf9e70c4ba89b
Christopher Little <hephistocles@gmail.com>
2015-07-28 21:46:00 +0100

    Finish barcode generation. Capped at 50 for performance.
    Turns out client-side is probably the wrong place for this. Although
    it all technically works (which I wasn't expecting to be possible),
    it's too slow to generate 1,600 barcodes on pageload. So this needs
    to be pushed to the server. Shouldn't be any problems with that -
    PDFKit generates a node stream anyway, and my fork of JSBarcode
    doesn't really use any browser stuff (apart from some jQuery which
    should be stripped away anyway imo)

--- assets/js/admin/barcode.controller.js
+++ assets/js/admin/barcode.controller.js
@@ -2,78 +2,152 @@
 angular.module('mabel.admin')
 	.controller("BarcodeController", BarcodeController);
 
-function BarcodeController() {
-	// JsBarcode(document.getElementById("barcode"), "78945 63214", {format:"UPC",displayValue:true, fontSize:20})
-	// JsBarcode(document.getElementById("barcode2"), "78945 63214", {format:"UPC",displayValue:true, fontSize:20})
-	// JsBarcode(document.getElementById("barcode3"), "78945 63214", {format:"UPC",displayValue:true, fontSize:20})
-	// JsBarcode(new JsBarcode.VectorRenderer(document.getElementById("barcode4")), "78945 63214", {format:"UPC",displayValue:true, fontSize:20})
+function BarcodeController($scope, APICaller) {
+	var vm = this;
 
-    var PDFRenderer = function PDFRenderer(doc, stickerWidth) {
-        this.doc = doc;
-        this.offX = 0;
-        this.offY = 0;
-        this.stickerWidth = stickerWidth;
-    };
-    PDFRenderer.prototype = Object.create(JsBarcode.Renderer.prototype);
-    PDFRenderer.prototype.coffX = function() {
-        return this.offX + this.stickerWidth/2 - this.width/2;
-    }
+	var PDFRenderer = function PDFRenderer(doc, stickerWidth, stickerHeight) {
+		this.doc = doc;
+		this.offX = 0;
+		this.offY = 0;
+		this.stickerWidth = stickerWidth;
+		this.stickerHeight = stickerHeight;
+	};
+	PDFRenderer.prototype = Object.create(JsBarcode.Renderer.prototype);
+	PDFRenderer.prototype.coffX = function() {
+		return this.offX + this.stickerWidth / 2 - this.width / 2;
+	}
 
-    PDFRenderer.prototype.size = function(w, h) {
-        this.width = w;
-        this.height = h;
-    };
-    PDFRenderer.prototype.rect = function(colour, x, y, w, h) {
-        this.doc.rect(mm(x + this.coffX()), mm(y + this.offY), mm(w), mm(h)).fill(colour)
-    }
-    PDFRenderer.prototype.text = function(text, x, y, size, font, align) {
+	PDFRenderer.prototype.size = function(w, h) {
+		this.width = w;
+		this.height = h;
+	};
+	PDFRenderer.prototype.rect = function(colour, x, y, w, h) {
+		this.doc.rect(mm(x + this.coffX()), mm(y + this.offY), mm(w), mm(h)).fill(colour)
+	}
+	PDFRenderer.prototype.text = function(text, x, y, size, font, align) {
 
-        if (align === "right") {
-            x -= this.width
-        } else if (align === "center") {
-            x -= this.width/2
-        }
-        this.doc
-            .font(font)
-            .fontSize(size)
-            .text(text, mm(x + this.coffX()), mm(y  - 7 + this.offY), {
-                width: mm(this.width),
-                align: align
-            })
-    }
+		if (align === "right") {
+			x -= this.width
+		} else if (align === "center") {
+			x -= this.width / 2
+		}
+		this.doc
+			.font(font)
+			.fontSize(size)
+			.text(text, mm(x + this.coffX()), mm(y - 8 + this.offY), {
+				width: mm(this.width),
+				align: align
+			})
+	}
 
-    function mm(n) {
-        return n*2.8346456692895527;
-    }
+	function mm(n) {
+		return n * 2.8346456692895527;
+	}
+
+	function drawStickerBase() {
+		// modelled after https://www.a4labels.com/products/transparent-gloss-labels-63-x-38mm/24696
+		for (var y = 0; y < 7; y++) {
+			for (var x = 0; x < 3; x += 1) {
+				doc.roundedRect(mm(7.75 + (63.5 + 2) * x), mm(15.5 + 38.1 * y), mm(63.5), mm(38.1), mm(1.5)).fillAndStroke("#EEEEEE", "#000000")
+			}
+		}
+	}
+
+	function drawSticker(r, doc, code, name, type) {
+		// barcodes
+		JsBarcode(r, code, {
+			format: "UPC",
+			displayValue: true,
+			font: "Helvetica",
+			fontSize: 10,
+			quite: 5,
+			width: 0.32,
+			height: r.stickerHeight
+		})
+
+		// labels
+		doc
+			.font("Helvetica")
+			.fontSize(14)
+			.text(name, mm(r.offX), mm(r.offY - 12), {
+				width: mm(r.stickerWidth),
+				align: "center"
+			})
+			.font("Helvetica-Bold")
+			.fontSize(10)
+			.text(type, mm(r.offX), mm(r.offY - 5), {
+				width: mm(r.stickerWidth),
+				align: "center"
+			})
+	}
+
+	function fiveChar(n) {
+		return ("00000" + n).slice(-5);
+	}
+
+	function makePDF(iframeID) {
+		// create a document and pipe to a blob
+		var doc = new PDFDocument({
+			size: "a4",
+			margin: 0
+		});
+
+		var stream = doc.pipe(blobStream());
+
+		var showGrid = false;
+		if (showGrid) drawStickerBase();
 
 
-	// create a document and pipe to a blob
-	var doc = new PDFDocument({size:"a4", margin:0});
+		var cols = 3,
+			rows = 7;
+		var w = 35,
+			h = 13,
+			offX = 7.75,
+			offY = 32,
+			mX = 2,
+			mY = 25.1;
+		var r = new PDFRenderer(doc, 63.5, 13);
+		r.stickerWidth = 63.5
+		for (var i = 0; i < 50; i++) {
+			
+			if (i > 0 && i % (cols * rows) === 0) {
+				doc.addPage()
+				if (showGrid) drawStickerBase();
+			}
+			
+			var x = i % cols;
+			var y = Math.floor(i / cols) % rows;
+			r.offY = offY + y * (h + mY);
+			r.offX = offX + x * (r.stickerWidth + mX);
 
-	var stream = doc.pipe(blobStream());
-    
-    // measurement grid
-    // for (var y = 0; y<297; y+=10) {
-    //     for (var x = (y%20==0?10:0); x<290; x+=20) {
-    //         doc.rect(mm(x), mm(y), mm(10), mm(10)).fill("#EEEEEE")
-    //     }
-    // }
+			var code = fiveChar(vm.allTickets[i].user_id) + "  " + fiveChar(vm.allTickets[i].id)
+			drawSticker(r, doc, code, allTickets[i].guest_name, allTickets[i].ticket_type_name);
+		}
+		console.log("Sent all commands");
+		// end and display the document in the iframe
+		doc.end();
+		console.log("Ended")
+		stream.on('finish', function() {
+			document.getElementById(iframeID).src = stream.toBlobURL('application/pdf');
+			console.log("Document written");
+		});
+		stream.on('pipe', function() {
+			console.log("Got data");
+		})
+	}
 
-    var cols = 4, rows = 9;
-    var w = 30, h = 20, offX = 5, offY = 10, mX = 0, mY = 10;
-    var r = new PDFRenderer(doc, w);
-    r.stickerWidth = 50
-    for (var i=0; i<50; i++) {
-        if (i>0 && i%(cols*rows)===0) doc.addPage()
-        var x = i%cols;
-        var y = Math.floor(i/cols) % rows;
-        r.offY = offY + y * (h + mY);
-        r.offX = offX + x * (r.stickerWidth + mX);
-        JsBarcode(r, "78945 63214", {format:"UPC",displayValue:true, font:"Courier", fontSize:10, quite:10, width:w/100, height: h})
-    }
-  // end and display the document in the iframe
-  doc.end();
-  stream.on('finish', function() {
-    document.getElementById("pdf").src = stream.toBlobURL('application/pdf');
-  });
+	console.log("Getting ticket data...")
+	APICaller.get("ticket/getAllDetailed", {},
+		function(err, data) {
+			if (err) return alert(err);
+			if (data.length < 1) return alert("No tickets found");
+			console.log("Ticket data obtained.")
+			vm.allTickets = _.filter(data, function(t) {
+				return t.status_id===2;
+			});
+			allTickets = vm.allTickets;
+			makePDF("pdf");
+		}
+	);
+
 }
