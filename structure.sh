914f7dca2a06aaf3ade2e03aab064fcfd99f7f34
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2015-08-22 10:56:26 +0100

    fix up scripts

---
+++ structure.sh
@@ -0,0 +1,10 @@
+mkdir -p ./public/dash
+mkdir -p ./public/book
+mkdir -p ./public/admin/event
+mkdir -p ./public/admin/tickets
+mkdir -p ./public/admin/transactions
+mkdir -p ./public/admin/users
+mkdir -p ./public/css
+mkdir -p ./public/js
+mkdir -p ./public/lib/css
+mkdir -p ./public/lib/js
