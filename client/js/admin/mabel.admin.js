c27795cddfef2257013eec0c9acfecb810386a1d
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2015-08-22 16:03:08 +0100

    add licenses to client js

--- client/js/admin/mabel.admin.js
+++ client/js/admin/mabel.admin.js
@@ -1,1 +1,7 @@
+/**
+ * Copyright (C) 2015  Mabel Ticketing 
+ * GNU General Public License v2.0
+ * https://github.com/mabelticketing/mabel/blob/master/LICENSE.txt
+ */
+
 angular.module('mabel.admin', ['mabel.shared', 'ngTable', 'ngTableExport', 'ngResource', 'ui.bootstrap']); 
--- client/js/admin/tickettypes.controller.js
+++ client/js/admin/tickettypes.controller.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) 2015  Mabel Ticketing 
+ * GNU General Public License v2.0
+ * https://github.com/mabelticketing/mabel/blob/master/LICENSE.txt
+ */
+
 angular.module('mabel.admin')
 	.controller("TicketTypesController", TicketTypesController);
 
