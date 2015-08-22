7a16a5617b46e1e02048e6a0a0cc9f8ccf8af470
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2015-08-22 10:56:26 +0100

    fix up scripts

--- jade.sh
+++ jade.sh
@@ -1,8 +1,8 @@
-mkdir -p ./assets && jade ./views/index.jade > ./assets/index.html && rm ./views/index.html
-mkdir -p ./assets/dash && jade ./views/dash.jade > ./assets/dash/index.html && rm ./views/dash.html
-mkdir -p ./assets/book && jade ./views/book.jade > ./assets/book/index.html && rm ./views/book.html
-mkdir -p ./assets/admin && jade ./views/admin/dash.jade > ./assets/admin/index.html && rm ./views/admin/dash.html
-mkdir -p ./assets/admin/event && jade ./views/admin/event.jade > ./assets/admin/event/index.html && rm ./views/admin/event.html
-mkdir -p ./assets/admin/tickets && jade ./views/admin/tickets.jade > ./assets/admin/tickets/index.html && rm ./views/admin/tickets.html
-mkdir -p ./assets/admin/transactions && jade ./views/admin/transactions.jade > ./assets/admin/transactions/index.html && rm ./views/admin/transactions.html
-mkdir -p ./assets/admin/users && jade ./views/admin/users.jade > ./assets/admin/users/index.html && rm ./views/admin/users.html
+mkdir -p ./public && jade ./views/index.jade > ./public/index.html && rm ./views/index.html
+mkdir -p ./public/dash && jade ./views/dash.jade > ./public/dash/index.html && rm ./views/dash.html
+mkdir -p ./public/book && jade ./views/book.jade > ./public/book/index.html && rm ./views/book.html
+mkdir -p ./public/admin && jade ./views/admin/dash.jade > ./public/admin/index.html && rm ./views/admin/dash.html
+mkdir -p ./public/admin/event && jade ./views/admin/event.jade > ./public/admin/event/index.html && rm ./views/admin/event.html
+mkdir -p ./public/admin/tickets && jade ./views/admin/tickets.jade > ./public/admin/tickets/index.html && rm ./views/admin/tickets.html
+mkdir -p ./public/admin/transactions && jade ./views/admin/transactions.jade > ./public/admin/transactions/index.html && rm ./views/admin/transactions.html
+mkdir -p ./public/admin/users && jade ./views/admin/users.jade > ./public/admin/users/index.html && rm ./views/admin/users.html
