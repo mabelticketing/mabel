0997c31199b95ab4ba2f8e7ecbb22fb5fcf3c48a
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2015-08-22 23:43:01 +0100

    tidy up events resource

--- src/api/routes/event.js
+++ src/api/routes/event.js
@@ -4,23 +4,38 @@
  * https://github.com/mabelticketing/mabel/blob/master/LICENSE.txt
  */
 
-var express = require("express");
+var express   = require("express");
 var apiRouter = require("../routes.js");
-var api = require("../api.js");
+var api       = require("../api.js");
+
 var router = express.Router({
 	mergeParams: true
 });
+
 module.exports = router;
 	
 router.route("/:id")
 	.get(
 		function(req, res) {
-			api.event.get(req.params.id, apiRouter.marshallResult(res));
+			api.event.get(req.params.id).then(function(rows) {
+				// TODO: test & fix
+				if (rows.length !== 1) throw new Error("err");
+				res.json(rows[0]);
+			}, function(err) {
+				console.log(err);
+				res.status(500).send(err);
+			});
 		}
 	)
-	.post(
+	.put(
 		apiRouter.checkAdmin(),
 		function(req, res) {
-			api.event.update(req.params.id, req.body, apiRouter.marshallResult(res));
+			api.event.update(req.params.id, req.body).then(function(rows) {
+				if (rows.length !== 1) throw new Error("err");
+				res.json(rows[0]);
+			}, function(err) {
+				console.log(err);
+				res.status(500).send(err);
+			});
 		}
 	);
