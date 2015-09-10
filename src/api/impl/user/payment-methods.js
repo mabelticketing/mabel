488524e8090f3bbc3541b7cbbaa3d54a392be610
Christopher Little <hephistocles@gmail.com>
2015-09-10 23:41:17 +0100

    Add new defaults file to go with new schema/for testing

--- src/api/impl/user/payment-methods.js
+++ src/api/impl/user/payment-methods.js
@@ -18,15 +18,16 @@ function payment_method(user_id) {
     // then seeing what access that groups give them (and then stripping duplicate methods out).
     // We also adjust the ticket limit by subtracting the number of tickets this user has bought
     // using this payment method.
+    // TODO: Refactor into a SQL view
     function get() {
         var sql = "SELECT id, name, description, ticket_limit - IFNULL(bought) AS ticket_limit \
-        FROM payment_method 
+        FROM payment_method \
             JOIN \
                 (SELECT DISTINCT(payment_method_id) FROM \
                     (SELECT * FROM user_group_membership WHERE user_id=?) A \
                         JOIN group_payment_method_access \
                         ON A.group_id=group_payment_method_access.group_id) B \
-            ON B.payment_method_id=id;
+            ON B.payment_method_id=id \
             LEFT JOIN \
                 (SELECT payment_method_id AS id, COUNT(*) AS bought FROM \
                     ticket WHERE user_id=? \
