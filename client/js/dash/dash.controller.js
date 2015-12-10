091c098e70003b2a242016766b21a23334b897a8
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2015-12-10 14:25:56 +0000

    make delete buttons work on dashboard, and sort out waiting list and booked lists

--- client/js/dash/dash.controller.js
+++ client/js/dash/dash.controller.js
@@ -4,7 +4,6 @@
  * https://github.com/mabelticketing/mabel/blob/master/LICENSE.txt
  */
 
-/* global moment */
 angular.module('mabel.dash')
 	.controller("DashController", DashController);
 
@@ -12,178 +11,156 @@ function DashController($scope, APICaller, User) {
 	var vm = this;
 
 	/*** DECLARATION ***/
-	//TODO: stop exposing vm on every controller!
-	e = vm;
 
-	// initialise scope vars 
+	// display dots when loading
+	vm.user = {name: "..."};
+
+	// initialise scope vars
 	vm.ticketsAvailable = [];
 	vm.ticketsBooked    = [];
-	vm.donationTickets  = [];
-	vm.transactions     = [];
-	vm.changedTickets   = [];
+	vm.waitingListTickets = [];
+	// vm.donationTickets  = [];
+	// vm.transactions     = [];
+	// vm.changedTickets   = [];
 
-	vm.totalValue        = 0;
-	vm.totalTransactions = 0;
+	// vm.totalValue        = 0;
+	// vm.totalTransactions = 0;
 
 	vm.waitingListLoading      = true;
-	vm.transactionsLoading     = true;
 	vm.ticketsBookedLoading    = true;
-	vm.ticketsAvailableLoading = true;
+	// vm.transactionsLoading     = true;
+	// vm.ticketsAvailableLoading = true;
 
-	vm.saveTicket          = saveTicket;
-	vm.saveTickets         = saveTickets;
-	vm.setChanged          = setChanged;
 	vm.cancelTicket        = cancelTicket;
-	vm.clearStatus         = clearStatus;
 	vm.cancelWaitingTicket = cancelWaitingTicket;
+	// vm.saveTicket          = saveTicket;
+	// vm.saveTickets         = saveTickets;
+	// vm.setChanged          = setChanged;
+	// vm.clearStatus         = clearStatus;
 
 	vm.saveBtnClass = "primary";
 
 	/*** INITIAL ACTION ***/
 
-	APICaller.get('ticket_type/available/1', function(err, data) {
-		if (err) return console.log(err);
-		// assign response to tickets array
-		vm.ticketsAvailable = data;
-		vm.ticketsAvailableLoading = false;
-	});
+	// TODO: do we need to show available tickets?
+	// APICaller.get('type', function(err, data) {
+	// 	if (err) return console.log(err);
+	// 	// assign response to tickets array
+	// 	vm.ticketsAvailable = data;
+	// 	vm.ticketsAvailableLoading = false;
+	// });
 
 	var userPromise = User.current();
 	userPromise.$promise.then(function(user) {
 		
-		APICaller.get('ticket/getByUser/' + user.id, function(err, data) {
-			if (err!==undefined && err!==null) return console.log(err);
+		vm.user = user;
+		
+		APICaller.get('user/' + user.id + '/ticket', function(err, data) {
+			if (err !== undefined && err !== null) return console.log(err);
+			console.log(data);
 
-			vm.ticketsBooked = data.real;
-
-			// I kind of want data.extra to represent a general "meta-ticket",
-			// but here I'm assuming exclusively donations - not very generalised.
-			vm.donationTickets = data.extra;
+			// sort tickets associated with user
+			for (var i=0; i<data.length; i++) {
+				if (data[i].status === 'PENDING_WL') {
+					vm.waitingListTickets.push(data[i]);
+				} else if (data[i].status === 'CANCELLED'
+					|| data[i].status === 'CANCELLED_WL'
+					|| data[i].status === 'INVALID') {
+					continue;
+				} else {
+					vm.ticketsBooked.push(data[i]);
+				}
+			}
 
 			vm.ticketsBookedLoading = false;
+			vm.waitingListLoading = false;
+
 			updateTotal();
 		});
 
-		APICaller.get('waiting_list/getByUser/' + user.id, function(err, data) {
-			if (err!==undefined && err!==null) return console.log(err);
-
-			vm.waitingListTickets = data;
-
-			vm.waitingListLoading = false;
-		});
-
-
-		APICaller.get('transaction/getByUser/'+user.id, function(err, data) {
-			if (err!==undefined && err!==null) return console.log(err);
-			vm.transactions = [];
-			vm.totalTransactions = 0;
-
-			for (var i=0; i<data.length; i++) {
-				vm.transactions.push({
-					time: moment.unix(data[i].transaction_time).format("Mo MMM LT"),
-					payment_method: data[i].payment_method,
-					notes: data[i].notes,
-					value: data[i].value
-				});
-				vm.totalTransactions += data[i].value;
-			}
-			
-			vm.transactionsLoading = false;
-		});
 	});
 	
 
 	/*** FUNCTION DEFINITIONS ***/
 
-	function saveTicket(ticket) {
-		ticket._status = "warning";
-		// TODO: This kind of get/save/delete thing is literally what $resources are for
-		APICaller.post('ticket/' + ticket.id, ticket, function(err) {
-				if (err!==undefined && err!==null) {
-					ticket._status = "danger";
-					alert(err);
-					return console.log(err);
-				}
-				ticket._status = "success";
-		});
+	// function saveTicket(ticket) {
+	// 	ticket._status = "warning";
+	// 	// TODO: This kind of get/save/delete thing is literally what $resources are for
+	// 	APICaller.post('ticket/' + ticket.id, ticket, function(err) {
+	// 			if (err!==undefined && err!==null) {
+	// 				ticket._status = "danger";
+	// 				alert(err);
+	// 				return console.log(err);
+	// 			}
+	// 			ticket._status = "success";
+	// 	});
 
-	}
+	// }
 
-	function setChanged(ticket) {
-		if (vm.changedTickets.indexOf(ticket) < 0) 
-			vm.changedTickets.push(ticket);
-	}
+	// function setChanged(ticket) {
+	// 	if (vm.changedTickets.indexOf(ticket) < 0) 
+	// 		vm.changedTickets.push(ticket);
+	// }
 
-	function saveTickets() {
-		vm.saveBtnClass = "warning";
+	// function saveTickets() {
+	// 	vm.saveBtnClass = "warning";
 		
-		APICaller.post('ticket/multi/', vm.changedTickets, function(err) {
-				if (err!==undefined && err!==null) {
-					vm.saveBtnClass = "danger";
-					alert(err);
-					return console.log(err);
-				}
-				alert("Thank you, your changes have been saved.");
-				vm.saveBtnClass = "success";
-		});
-	}
+	// 	APICaller.post('ticket/multi/', vm.changedTickets, function(err) {
+	// 			if (err!==undefined && err!==null) {
+	// 				vm.saveBtnClass = "danger";
+	// 				alert(err);
+	// 				return console.log(err);
+	// 			}
+	// 			alert("Thank you, your changes have been saved.");
+	// 			vm.saveBtnClass = "success";
+	// 	});
+	// }
 
-	function clearStatus(ticket) {
-		ticket._status = "";
-	}
+	// function clearStatus(ticket) {
+	// 	ticket._status = "";
+	// }
 
 	function cancelTicket(ticket) {
 		if (window.confirm("Do you really want to cancel this ticket?")) { 
-			APICaller.del('ticket/' + ticket.id, function(err) {
+			APICaller.del('user/' + vm.user.id + '/ticket/' + ticket.id, function(err) {
 				if (err!==undefined && err!==null) return console.log(err);
 				
 				for (var i=0; i<vm.ticketsBooked.length; i++) {
 					if (vm.ticketsBooked[i] === ticket) {
+						// removes deleted ticket from array
 						vm.ticketsBooked.splice(i, 1);
 						break;
 					}
 				}
-
 				updateTotal();
 			});
-			if (vm.donationTickets.length > 0) {
-				// TODO: this is pretty hacky
-				// delete one of the donation tickets 
-				var toDelete = vm.donationTickets.pop();
-				APICaller.del('/ticket/' + toDelete.id, function(err) {
-					if (err && err!==null) return console.log(err);
-
-					updateTotal();
-				});
-			}
 		}
 	}
-	function cancelWaitingTicket(ticket) {
 
-		if (window.confirm("Do you really want to leave the waiting list?")) { 
-			APICaller.del('waiting_list/' + ticket.id, function(err) {
+	function cancelWaitingTicket(ticket) {
+		if (window.confirm("Do you really want to leave the waiting list?")) {
+			APICaller.del('user/' + vm.user.id + '/ticket/' + ticket.id, function(err) {
 				if (err!==undefined && err!==null) return console.log(err);
 				
 				for (var i=0; i<vm.waitingListTickets.length; i++) {
 					if (vm.waitingListTickets[i] === ticket) {
+						// removes deleted ticket from array
 						vm.waitingListTickets.splice(i, 1);
 						break;
 					}
 				}
+
 			});
 		}
 	}
+
 	function updateTotal() {
+		// updates total ticket price
 
 		vm.totalValue = 0;
 		for (var i = 0; i<vm.ticketsBooked.length; i++) {
-			vm.totalValue += vm.ticketsBooked[i].price;
+			vm.totalValue += vm.ticketsBooked[i].transaction_value;
 		}
-		vm.donationValue = 0;
-		for (i = 0; i<vm.donationTickets.length; i++) {
-			vm.donationValue += vm.donationTickets[i].price;
-		}
-		vm.totalValue += vm.donationValue;
 
 	}
 
