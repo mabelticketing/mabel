c538d4421b04e141c1872f1cf7bd98dcd77ce0f5
Thomas Le Feuvre <tjlefeuvre@gmail.com>
2016-02-11 22:10:43 +0000

    add grad membership sql file

---
+++ sql/grad-mems.sql
@@ -0,0 +1,23 @@
+
+
+insert into user_group_membership (user_id, group_id) values(7,3);
+insert into user_group_membership (user_id, group_id) values(8,3);
+insert into user_group_membership (user_id, group_id) values(9,3);
+insert into user_group_membership (user_id, group_id) values(10,3);
+insert into user_group_membership (user_id, group_id) values(11,3);
+insert into user_group_membership (user_id, group_id) values(12,3);
+insert into user_group_membership (user_id, group_id) values(13,3);
+insert into user_group_membership (user_id, group_id) values(14,3);
+insert into user_group_membership (user_id, group_id) values(15,3);
+insert into user_group_membership (user_id, group_id) values(16,3);
+insert into user_group_membership (user_id, group_id) values(17,3);
+insert into user_group_membership (user_id, group_id) values(18,3);
+insert into user_group_membership (user_id, group_id) values(19,3);
+insert into user_group_membership (user_id, group_id) values(20,3);
+insert into user_group_membership (user_id, group_id) values(21,3);
+insert into user_group_membership (user_id, group_id) values(22,3);
+insert into user_group_membership (user_id, group_id) values(23,3);
+insert into user_group_membership (user_id, group_id) values(24,3);
+insert into user_group_membership (user_id, group_id) values(25,3);
+insert into user_group_membership (user_id, group_id) values(26,3);
+
