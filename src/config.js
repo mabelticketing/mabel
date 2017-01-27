a67a9e45d45bce0b87b33dde403cb8b84b71d9db
Christopher Little <christopher@littlehq.uk>
2017-01-27 01:03:51 +0000

    Modify text to match what we expect in 2017 and small schema changes

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 /**
- * Copyright (C) 2015  Mabel Ticketing 
+ * Copyright (C) 2015  Mabel Ticketing
  * GNU General Public License v2.0
  * https://github.com/mabelticketing/mabel/blob/master/LICENSE.txt
  */
 
 module.exports = {
-    port        : 3008,
-    base_url    : "http://localhost:3008",
+    port        : 3011,
+    base_url    : "http://tickets.emmamayball.com",
     db_host     : "localhost",
     db_password : "LockDown1",
-    db_user     : "mabel",
-    db_db       : "mabel_test",
-    jwt_secret  : "jwt-pass",
-    mailgun_api_key: "",
+    db_user     : "root",
+    db_db       : "mabel",
+    jwt_secret  : "jwt-psdfsdfass",
+    mailgun_api_key: "key-b2441b341e4124c1efaff49c5216dcbe",
   	admin_groups: [1],
   	donation_value: 2,
     external_keys: {
