6c62f5762293ab57d43708e7edf1c58717b701b3
Christopher Little <hephistocles@gmail.com>
2017-06-06 16:43:36 +0000

    Switch to jsPDF for mega speed

--- client/js/admin/barcode.controller.js
+++ client/js/admin/barcode.controller.js
@@ -11,145 +11,149 @@ angular.module('mabel.admin')
 function BarcodeController($scope, APICaller) {
 	var vm = this;
 
-	var PDFRenderer = function PDFRenderer(doc, stickerWidth, stickerHeight) {
-		this.doc = doc;
-		this.offX = 0;
-		this.offY = 0;
-		this.stickerWidth = stickerWidth;
-		this.stickerHeight = stickerHeight;
-	};
-	PDFRenderer.prototype = Object.create(JsBarcode.Renderer.prototype);
-	PDFRenderer.prototype.coffX = function() {
-		return this.offX + this.stickerWidth / 2 - this.width / 2;
-	}
+	function EANchecksum(number){
+		var result = 0;
+		
+		for(var i=0;i<12;i+=2){result+=parseInt(number[i]);}
+		for(var j=1;j<12;j+=2){result+=parseInt(number[j])*3;}
 
-	PDFRenderer.prototype.size = function(w, h) {
-		this.width = w;
-		this.height = h;
-	};
-	PDFRenderer.prototype.rect = function(colour, x, y, w, h) {
-		this.doc.rect(mm(x + this.coffX()), mm(y + this.offY), mm(w), mm(h)).fill(colour)
-	}
-	PDFRenderer.prototype.text = function(text, x, y, size, font, align) {
-
-		if (align === "right") {
-			x -= this.width
-		} else if (align === "center") {
-			x -= this.width / 2
-		}
-		this.doc
-			.font(font)
-			.fontSize(size)
-			.text(text, mm(x + this.coffX()), mm(y - 8 + this.offY), {
-				width: mm(this.width),
-				align: align
-			})
-	}
-
-	function mm(n) {
-		return n * 2.8346456692895527;
-	}
-
-	function drawStickerBase() {
-		// modelled after https://www.a4labels.com/products/transparent-gloss-labels-63-x-38mm/24696
-		for (var y = 0; y < 7; y++) {
-			for (var x = 0; x < 3; x += 1) {
-				doc.roundedRect(mm(7.75 + (63.5 + 2) * x), mm(15.5 + 38.1 * y), mm(63.5), mm(38.1), mm(1.5)).fillAndStroke("#EEEEEE", "#000000")
-			}
-		}
-	}
-
-	function drawSticker(r, doc, code, name, type) {
-		// barcodes
-		JsBarcode(r, code, {
-			format: "UPC",
-			displayValue: true,
-			font: "Helvetica",
-			fontSize: 10,
-			quite: 5,
-			width: 0.32,
-			height: r.stickerHeight
-		})
-
-		// labels
-		doc
-			.font("Helvetica")
-			.fontSize(14)
-			.text(name, mm(r.offX), mm(r.offY - 12), {
-				width: mm(r.stickerWidth),
-				align: "center"
-			})
-			.font("Helvetica-Bold")
-			.fontSize(10)
-			.text(type, mm(r.offX), mm(r.offY - 5), {
-				width: mm(r.stickerWidth),
-				align: "center"
-			})
+		return (10 - (result % 10)) % 10;
 	}
 
 	function fiveChar(n) {
 		return ("00000" + n).slice(-5);
 	}
+	
+	function ticketType(n) {
+	  switch(n) {
+		  case 1:
+		  case 2:
+		  case 3:
+		  case 4:
+		  case 5:
+		  case 6:
+		  	return "Standard";
+	  }
+	}
 
 	function makePDF(iframeID) {
+		function drawStickerBase(doc) {
+			// modelled after https://www.a4labels.com/products/transparent-gloss-labels-63-x-38mm/24696
+			for (var y = 0; y < 7; y++) {
+				for (var x = 0; x < 3; x += 1) {
+					doc
+						.setFillColor(230)
+						.setDrawColor(0)
+						.roundedRect(7.75 + (63.5 + 2) * x, 15.5 + 38.1 * y, 63.5, 38.1, 1.5, 1.5, 'DF');
+				}
+			}
+		}
+
+	  	function drawSticker(r, doc, code, name, type, stickerWidth, stickerHeight) {
+
+		  	// tolerate any spaces (just strip them out)
+		  	var EANnumber = (code + "").replace(/\s/g, "");
+		  	// Add an EAN country code and prefix
+		  	EANnumber = "00" + EANnumber;
+		  	// add an EAN check digit
+		  	EANnumber += EANchecksum(EANnumber);
+
+		  	// barcodes
+		  	var data = {};
+		  	JsBarcode(data, EANnumber, {
+			  	format: "EAN13",
+			  	font: "Helvetica",
+			  	fontSize: 10,
+		  	});
+
+		  	var count = 0;
+		  	var slack = 10;
+		  	var barCodeWidth = stickerWidth - slack;
+		  	var barCodeHeight = stickerHeight;
+		  	// there seems to be 95 strips for our codes?
+		  	var barWidth = barCodeWidth/95;
+
+		  	for (var i=0; i<data.encodings.length; i++) {
+			  	var section = data.encodings[i];
+			  	// discard null initial blocks
+			  	if (parseInt(section.data) === 0 && i===0)
+			  	  continue;
+
+			  	for (var j=0; j<section.data.length; j++) {
+				  	var bar = section.data[j] === "1";
+				  	count ++;
+					var x = slack/2 + barWidth*count;
+				  	if (bar)
+						doc.rect(x + r.offX, r.offY, barWidth, barCodeHeight, 'F');
+				  	  // r.rect(doc, "black", slack/2 + barWidth*count, 0, barWidth, barCodeHeight);
+			  	}
+		  	}
+
+		  	// labels
+		  	doc
+				.setLineWidth(stickerWidth)
+			  	.setFont("helvetica", "normal")
+			  	.setFontSize(14)
+			  	.text(name, stickerWidth/2 + r.offX,r.offY - 8, "center")
+			  	.setFont("helvetica", "bold")
+			  	.setFontSize(10)
+			  	.text(type || "Unknown",  stickerWidth/2 + r.offX, r.offY -3, "center")
+			  	.setFont("courier", "normal")
+			  	.setFontSize(10)
+			  	.text(code,  stickerWidth/2 + r.offX, r.offY + 17, "center");
+	  	}
+
+
 		// create a document and pipe to a blob
-		var doc = new PDFDocument({
-			size: "a4",
-			margin: 0
-		});
-
-		var stream = doc.pipe(blobStream());
-
+		var doc = new jsPDF();
+		
 		var showGrid = false;
-		if (showGrid) drawStickerBase();
-
+		if (showGrid) drawStickerBase(doc);
+				doc.setDrawColor(0).setFillColor(0);
 
 		var cols = 3,
 			rows = 7;
-		var w = 35,
-			h = 13,
+		var h = 13,
 			offX = 7.75,
 			offY = 32,
 			mX = 2,
 			mY = 25.1;
-		var r = new PDFRenderer(doc, 63.5, 13);
-		r.stickerWidth = 63.5
-		for (var i = 0; i < 50; i++) {
+		var r = {offX: 0, offY: 0};
+		var stickerWidth = 63.5;
+		var stickerHeight = 13;
+
+		for (var i = 0; i < vm.allTickets.length; i++) {
 			
 			if (i > 0 && i % (cols * rows) === 0) {
-				doc.addPage()
-				if (showGrid) drawStickerBase();
+				doc.addPage();
+				if (showGrid) drawStickerBase(doc);
+				doc.setDrawColor(0).setFillColor(0);
 			}
 			
 			var x = i % cols;
 			var y = Math.floor(i / cols) % rows;
 			r.offY = offY + y * (h + mY);
-			r.offX = offX + x * (r.stickerWidth + mX);
+			r.offX = offX + x * (stickerWidth + mX);
 
-			var code = fiveChar(vm.allTickets[i].user_id) + "  " + fiveChar(vm.allTickets[i].id)
-			drawSticker(r, doc, code, allTickets[i].guest_name, allTickets[i].ticket_type_name);
+			var code = fiveChar(vm.allTickets[i].user_id) + "  " + fiveChar(vm.allTickets[i].id);
+			drawSticker(r, doc, code, vm.allTickets[i].guest_name, ticketType(vm.allTickets[i].ticket_type_id), stickerWidth, stickerHeight);
 		}
+
 		console.log("Sent all commands");
+
 		// end and display the document in the iframe
-		doc.end();
-		console.log("Ended")
-		stream.on('finish', function() {
-			document.getElementById(iframeID).src = stream.toBlobURL('application/pdf');
-			console.log("Document written");
-		});
-		stream.on('pipe', function() {
-			console.log("Got data");
-		})
+		document.getElementById(iframeID).src = doc.output('bloburl');
+		console.log("Ended");
 	}
 
 	console.log("Getting ticket data...")
-	APICaller.get("ticket/getAllDetailed", {},
+	APICaller.get("ticket", {},
 		function(err, data) {
-			if (err) return alert(err);
+			if (err) return console.error(err);
 			if (data.length < 1) return alert("No tickets found");
 			console.log("Ticket data obtained.")
 			vm.allTickets = _.filter(data, function(t) {
-				return t.status_id===2;
+				return t.status==="CONFIRMED";
 			});
 			allTickets = vm.allTickets;
 			makePDF("pdf");
